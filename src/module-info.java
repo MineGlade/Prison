91f07035c74805bbdac4c375bb40b66e8dfc6b41
Robin <robinslot@hotmail.nl>
2019-08-19 22:34:06 +0200

    delete module info file

--- src/module-info.java
+++
@@ -1,7 +0,0 @@
-module prison {
-	exports xyz.derkades.prisonglade;
-	exports xyz.derkades.prisonglade.themes;
-	exports xyz.derkades.prisonglade.mines;
-	exports xyz.derkades.prisonglade.mines.farm;
-	exports xyz.derkades.prisonglade.mines.nether;
-}
