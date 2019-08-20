6c02eb03e3d7044bc5624a21f0f5c76c116cf427
Funlolxxl <lolxxl434@gmail.com>
2019-08-20 20:11:37 +0100

    added blocks to the mines under stone theme

--- src/xyz/derkades/prisonglade/mines/stone/StoneD.java
+++ src/xyz/derkades/prisonglade/mines/stone/StoneD.java
@@ -1,6 +1,9 @@
 package xyz.derkades.prisonglade.mines.stone;
 
+import org.bukkit.Material;
+
 import xyz.derkades.prisonglade.mines.PrisonMine;
+import xyz.derkades.prisonglade.mines.PrisonMineFactory;
 
 public class StoneD extends StoneLevel {
 
@@ -11,7 +14,18 @@ public class StoneD extends StoneLevel {
 
 	@Override
 	public PrisonMine getMine() {
-		return null;
+		return new PrisonMineFactory(this.getWorld(), 0, 0, 0, 0, 0, 0, Material.STONE)
+				.addBlock(Material.COBBLESTONE, 0.2f)
+				.addBlock(Material.STONE, 5, 0.15f)
+				.addBlock(Material.STONE, 1, 0.07f)
+				.addBlock(Material.STONE, 3, 0.07f)
+				.addBlock(Material.GRAVEL, 0.07f)
+				.addBlock(Material.MOSSY_COBBLESTONE, 0.04f)
+				.addBlock(Material.DIRT, 0.03f)
+				.addBlock(Material.WOOD, 0, 0.04f)
+				.addBlock(Material.WEB, 0.035f)
+				.addBlock(Material.COAL_ORE, 0.025f)
+				.create();
 	}
 
 }
