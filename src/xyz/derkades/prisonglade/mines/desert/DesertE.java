70b1d923f02b2cc4f61dcdea86070353e8c34d31
Funlolxxl <lolxxl434@gmail.com>
2019-08-20 14:48:32 +0100

    Added desert mines t/m E

---
+++ src/xyz/derkades/prisonglade/mines/desert/DesertE.java
@@ -0,0 +1,17 @@
+package xyz.derkades.prisonglade.mines.desert;
+
+import xyz.derkades.prisonglade.mines.PrisonMine;
+
+public class DesertE extends DesertLevel {
+
+	@Override
+	public char getChar() {
+		return 'E';
+	}
+
+	@Override
+	public PrisonMine getMine() {
+		return null;
+	}
+
+}
