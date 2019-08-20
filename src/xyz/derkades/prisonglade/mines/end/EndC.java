bcbfe4097407cab22ba3373773cf3b86fc1987e7
Funlolxxl <lolxxl434@gmail.com>
2019-08-20 14:17:39 +0100

    added 4 end levels

---
+++ src/xyz/derkades/prisonglade/mines/end/EndC.java
@@ -0,0 +1,17 @@
+package xyz.derkades.prisonglade.mines.end;
+
+import xyz.derkades.prisonglade.mines.PrisonMine;
+
+public class EndC extends EndLevel {
+
+	@Override
+	public char getChar() {
+		return 'C';
+	}
+
+	@Override
+	public PrisonMine getMine() {
+		return null;
+	}
+
+}
---
+++ src/xyz/derkades/prisonglade/mines/end/EndE.java
@@ -0,0 +1,17 @@
+package xyz.derkades.prisonglade.mines.end;
+
+import xyz.derkades.prisonglade.mines.PrisonMine;
+
+public class EndE extends EndLevel {
+
+	@Override
+	public char getChar() {
+		return 'E';
+	}
+
+	@Override
+	public PrisonMine getMine() {
+		return null;
+	}
+
+}
