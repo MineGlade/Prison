b24b0f46e44ea6318443af852a700ad7c750defa
Funlolxxl <lolxxl434@gmail.com>
2019-08-20 14:51:48 +0100

    Added water mines t/mE

---
+++ src/xyz/derkades/prisonglade/mines/water/WaterD.java
@@ -0,0 +1,17 @@
+package xyz.derkades.prisonglade.mines.water;
+
+import xyz.derkades.prisonglade.mines.PrisonMine;
+
+public class WaterD extends WaterLevel {
+
+	@Override
+	public char getChar() {
+		return 'D';
+	}
+
+	@Override
+	public PrisonMine getMine() {
+		return null;
+	}
+
+}
---
+++ src/xyz/derkades/prisonglade/mines/water/WaterE.java
@@ -0,0 +1,17 @@
+package xyz.derkades.prisonglade.mines.water;
+
+import xyz.derkades.prisonglade.mines.PrisonMine;
+
+public class WaterE extends WaterLevel {
+
+	@Override
+	public char getChar() {
+		return 'E';
+	}
+
+	@Override
+	public PrisonMine getMine() {
+		return null;
+	}
+
+}
