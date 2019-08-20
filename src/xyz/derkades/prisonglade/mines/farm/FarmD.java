9190f3c556190d2dccb6000013c1c4a13f6af639
Funlolxxl <lolxxl434@gmail.com>
2019-08-20 13:45:52 +0100

    Blocks added for all farm lvls t/m J

--- src/xyz/derkades/prisonglade/mines/farm/FarmD.java
+++ src/xyz/derkades/prisonglade/mines/farm/FarmD.java
@@ -1,6 +1,9 @@
 package xyz.derkades.prisonglade.mines.farm;
 
+import org.bukkit.Material;
+
 import xyz.derkades.prisonglade.mines.PrisonMine;
+import xyz.derkades.prisonglade.mines.PrisonMineFactory;
 
 public class FarmD extends FarmLevel {
 
@@ -11,7 +14,25 @@ public class FarmD extends FarmLevel {
 
 	@Override
 	public PrisonMine getMine() {
-		return null;
+		return new PrisonMineFactory(this.getWorld(), 10, 43, 7, -10, 63, 27, Material.DIRT)
+				.addBlock(Material.GRAVEL, 0.2f)
+				.addBlock(Material.CLAY, 0.1f)
+				.addBlock(Material.LEAVES, 0, 0.01f)
+				.addBlock(Material.LEAVES, 2, 0.01f)
+				.addBlock(Material.LEAVES, 1, 0.01f)
+				.addBlock(Material.LEAVES, 3, 0.01f)
+				.addBlock(Material.LEAVES_2, 0, 0.01f)
+				.addBlock(Material.LEAVES_2, 1, 0.01f)
+				.addBlock(Material.WOOD, 0, 0.035f)
+				.addBlock(Material.WOOD, 2, 0.03f)
+				.addBlock(Material.WOOD, 1, 0.035f)
+				.addBlock(Material.WOOD, 3, 0.035f)
+				.addBlock(Material.WOOD, 4, 0.03f)
+				.addBlock(Material.WOOD, 5, 0.025f)
+				.addBlock(Material.LOG, 0, 0.02f)
+				.addBlock(Material.LOG, 2, 0.015f)
+				.addBlock(Material.LOG, 1, 0.01f)
+				.create();
 	}
 
 }
--- src/xyz/derkades/prisonglade/mines/farm/FarmH.java
+++ src/xyz/derkades/prisonglade/mines/farm/FarmH.java
@@ -1,6 +1,9 @@
 package xyz.derkades.prisonglade.mines.farm;
 
+import org.bukkit.Material;
+
 import xyz.derkades.prisonglade.mines.PrisonMine;
+import xyz.derkades.prisonglade.mines.PrisonMineFactory;
 
 public class FarmH extends FarmLevel {
 
@@ -11,7 +14,28 @@ public class FarmH extends FarmLevel {
 
 	@Override
 	public PrisonMine getMine() {
-		return null;
+		return new PrisonMineFactory(this.getWorld(), 10, 43, 7, -10, 63, 27, Material.DIRT)
+				.addBlock(Material.GRAVEL, 0.2f)
+				.addBlock(Material.CLAY, 0.1f)
+				.addBlock(Material.LEAVES, 0, 0.01f)
+				.addBlock(Material.LEAVES, 2, 0.01f)
+				.addBlock(Material.LEAVES, 1, 0.01f)
+				.addBlock(Material.LEAVES, 3, 0.01f)
+				.addBlock(Material.LEAVES_2, 0, 0.01f)
+				.addBlock(Material.LEAVES_2, 1, 0.01f)
+				.addBlock(Material.WOOD, 0, 0.015f)
+				.addBlock(Material.WOOD, 2, 0.01f)
+				.addBlock(Material.WOOD, 1, 0.015f)
+				.addBlock(Material.WOOD, 3, 0.015f)
+				.addBlock(Material.WOOD, 4, 0.015f)
+				.addBlock(Material.WOOD, 5, 0.015f)
+				.addBlock(Material.LOG, 0, 0.04f)
+				.addBlock(Material.LOG, 2, 0.035f)
+				.addBlock(Material.LOG, 1, 0.03f)
+				.addBlock(Material.LOG, 3, 0.025f)
+				.addBlock(Material.LOG, 4, 0.02f)
+				.addBlock(Material.LOG, 5, 0.015f)
+				.create();
 	}
 
 }
