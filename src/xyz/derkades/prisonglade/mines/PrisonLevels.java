f8e8ee04a92c172aa9985c4976ca52c2ab7b14b9
Robin <robinslot@hotmail.nl>
2019-08-21 02:20:33 +0200

    Register mines

--- src/xyz/derkades/prisonglade/mines/PrisonLevels.java
+++ src/xyz/derkades/prisonglade/mines/PrisonLevels.java
@@ -1,6 +1,20 @@
 package xyz.derkades.prisonglade.mines;
 
+import xyz.derkades.prisonglade.mines.cold.ColdA;
+import xyz.derkades.prisonglade.mines.cold.ColdB;
+import xyz.derkades.prisonglade.mines.cold.ColdC;
+import xyz.derkades.prisonglade.mines.cold.ColdD;
+import xyz.derkades.prisonglade.mines.cold.ColdE;
+import xyz.derkades.prisonglade.mines.desert.DesertA;
+import xyz.derkades.prisonglade.mines.desert.DesertB;
+import xyz.derkades.prisonglade.mines.desert.DesertC;
+import xyz.derkades.prisonglade.mines.desert.DesertD;
+import xyz.derkades.prisonglade.mines.desert.DesertE;
 import xyz.derkades.prisonglade.mines.end.EndA;
+import xyz.derkades.prisonglade.mines.end.EndB;
+import xyz.derkades.prisonglade.mines.end.EndC;
+import xyz.derkades.prisonglade.mines.end.EndD;
+import xyz.derkades.prisonglade.mines.end.EndE;
 import xyz.derkades.prisonglade.mines.farm.FarmA;
 import xyz.derkades.prisonglade.mines.farm.FarmB;
 import xyz.derkades.prisonglade.mines.farm.FarmC;
@@ -21,6 +35,16 @@ import xyz.derkades.prisonglade.mines.nether.NetherG;
 import xyz.derkades.prisonglade.mines.nether.NetherH;
 import xyz.derkades.prisonglade.mines.nether.NetherI;
 import xyz.derkades.prisonglade.mines.nether.NetherJ;
+import xyz.derkades.prisonglade.mines.stone.StoneA;
+import xyz.derkades.prisonglade.mines.stone.StoneB;
+import xyz.derkades.prisonglade.mines.stone.StoneC;
+import xyz.derkades.prisonglade.mines.stone.StoneD;
+import xyz.derkades.prisonglade.mines.stone.StoneE;
+import xyz.derkades.prisonglade.mines.water.WaterA;
+import xyz.derkades.prisonglade.mines.water.WaterB;
+import xyz.derkades.prisonglade.mines.water.WaterC;
+import xyz.derkades.prisonglade.mines.water.WaterD;
+import xyz.derkades.prisonglade.mines.water.WaterE;
 
 public class PrisonLevels {
 
@@ -48,6 +72,34 @@ public class PrisonLevels {
 			new NetherJ(),
 
 			new EndA(),
+			new EndB(),
+			new EndC(),
+			new EndD(),
+			new EndE(),
+
+			new DesertA(),
+			new DesertB(),
+			new DesertC(),
+			new DesertD(),
+			new DesertE(),
+
+			new ColdA(),
+			new ColdB(),
+			new ColdC(),
+			new ColdD(),
+			new ColdE(),
+
+			new WaterA(),
+			new WaterB(),
+			new WaterC(),
+			new WaterD(),
+			new WaterE(),
+
+			new StoneA(),
+			new StoneB(),
+			new StoneC(),
+			new StoneD(),
+			new StoneE(),
 	};
 
 }
