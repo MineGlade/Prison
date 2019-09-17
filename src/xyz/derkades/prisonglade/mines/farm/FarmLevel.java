88f9735d7bf6e731f2aedf8185dfd2223e5300c2
Robin <robinslot@hotmail.nl>
2019-09-17 12:03:54 +0200

    Add example XP settings for farm

--- src/xyz/derkades/prisonglade/mines/farm/FarmLevel.java
+++ src/xyz/derkades/prisonglade/mines/farm/FarmLevel.java
@@ -1,13 +1,24 @@
 package xyz.derkades.prisonglade.mines.farm;
 
+import org.bukkit.Material;
+
 import xyz.derkades.prisonglade.mines.PrisonLevel;
 import xyz.derkades.prisonglade.themes.Theme;
+import xyz.derkades.prisonglade.xp.ExpSettings;
 
 public abstract class FarmLevel extends PrisonLevel {
 
 	@Override
 	public Theme getTheme() {
 		return Theme.FARM;
+	}
+
+	@Override
+	public ExpSettings getExpSettings() {
+		return new ExpSettings()
+				.addBlock(Material.DIRT, 1)
+				.addBlock(Material.WOOD, 2)
+				;
 	}
 
 }
